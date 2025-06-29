576e02da56dc72e29af7ff3bc026586448d189ec
Programmer <Programmer@example.com>
2025-06-29 10:51:56 +0530

    added index.js

---
+++ backend/api/index.js
@@ -0,0 +1,8 @@
+// api/index.js
+import app from "../server";
+import { createServer } from "http";
+
+export default function handler(req, res) {
+  const server = createServer(app);
+  server.emit("request", req, res);
+}
